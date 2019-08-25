14c477654b85e70d6e9b0f9fcfed5453cd4197e4
boyunjjang1 <boyunqueen@gmail.com>
2019-08-25 22:30:42 +0900

    ADD sec01.ex02 prepareStatement Connect DB

--- pro07/src/sec01/ex02/MemberDAO.java
+++ pro07/src/sec01/ex02/MemberDAO.java
@@ -1,65 +1,82 @@
-package sec01.ex01;
+package sec01.ex02;
+
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
+// 확장 패키지 javax
+// javax.naming ? 네임 서베스에 액세스하기 위한 클래스 및 인터페이스를 제공
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 
 public class MemberDAO {
 	private static final String driver = "oracle.jdbc.driver.OracleDriver";
 	private static final String url = "jdbc:oracle:thin:@localhost:1521:XE";
 	private static final String user = "scott";
 	private static final String pwd = "triger";
-	private Connection con;
-	private Statement stmt;
 	
-	public List<MemberVO> listMembers()
-	{
+	private Connection con;
+	private PreparedStatement pstmt;
+	private DataSource dataFactory;
+	
+	public List<MemberVO> listMembers(){
 		List<MemberVO> list = new ArrayList<MemberVO>();
 		try {
-			connDB(); 
+			connDB();
+			// con = dataFactory.getConnection();
 			String query = "select * from t_member";
-			System.out.println(query);
-			// statement를 활용한 데이터베이스 연동
-			// sql문을 컴파일함 --> 속도가 느림
-			ResultSet rs = stmt.executeQuery(query); // sql문으로 회원정보 조회
+			System.out.println("prepareSatement:" + query);
 			
-			while(rs.next()) {
+			pstmt = con.prepareStatement(query); 
+			//prepareStatement 메서드에 sql문을 전달하여 prepareStatement 객체를 생성
+			ResultSet rs = pstmt.executeQuery();
+			// executeQuery() 메서드를 호출해 미리 설정한 sql문을 실행
+			
+			while(rs.next())
+			{
 				String id = rs.getString("id");
 				String pwd = rs.getString("pwd");
 				String name = rs.getString("name");
-				String email = rs.getString("EMIAL");
+				String email = rs.getString("emial");
 				Date joinDate = rs.getDate("joinDate");
 				MemberVO vo = new MemberVO();
-				vo.setId(id); // 각 칼럼값을 객체의 속성으로 지정
+				vo.setId(id);
 				vo.setPwd(pwd);
-				vo.setName(name);
 				vo.setEmail(email);
 				vo.setJoinDate(joinDate);
 				list.add(vo);
 			}
+			
 			rs.close();
-			stmt.close();
+			pstmt.close();
 			con.close();
-		} catch(Exception e) {
+		} catch (Exception e)
+		{
 			e.printStackTrace();
 		}
-		return list; // 레코드의 개수만큼 MemberVO객체를 저장한 ArrayList를 반환
+		
+		return list;
 	}
 	
-	private void connDB() {
-		try {
+	private void connDB()
+	{
+		try
+		{
 			Class.forName(driver);
 			System.out.println("Oracle 드라이버 로딩 성공");
 			con = DriverManager.getConnection(url, user, pwd);
 			System.out.println("Connection 생성 성공");
-			stmt = con.createStatement();
-			System.out.println("Statement 생성 성공");
-		} catch (Exception e) {
+		} catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 	}
+	
 
 }
